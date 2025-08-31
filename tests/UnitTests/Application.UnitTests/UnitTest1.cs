cb8c8739ee2d4c193a42be1007eab4c3c6fff914
NimbleGroot <193902962+NimbleGroot@users.noreply.github.com>
2025-08-31 22:59:02 +1000

    * Initial commit

---
+++ tests/UnitTests/Application.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Application.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ tests/UnitTests/Infrastructure.UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace Infrastructure.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
